99ee4e38c3527026621518cd61ca492db65a913a
huibing <13728166372@163.com>
2023-02-15 00:56:50 +0800

    First commit

---
+++ chatbot.py
@@ -0,0 +1,38 @@
+'''
+Author: huibing 13728166372@163.com
+Date: 2023-02-14 20:18:22
+LastEditors: huibing 13728166372@163.com
+LastEditTime: 2023-02-15 00:39:15
+FilePath: /undefined/Users/huibinglin/comp7940-lab3/chatbot.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+
+## chatbot.py
+import telegram 
+from telegram.ext import Updater, MessageHandler, Filters
+# The messageHandler is used for all message updates
+import configparser
+import logging
+def main():
+    # Load your token and create an Updater for your Bot
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+    dispatcher = updater.dispatcher
+    # You can set this logging module, so you will know when and why things do not work as expected
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - % (message)s',
+                        level=logging.INFO)
+    # register a dispatcher to handle message: here we register an echo dispatcher
+    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    dispatcher.add_handler(echo_handler)
+    # To start the bot:
+    updater.start_polling()
+    updater.idle()
+
+def echo(update, context):
+    reply_message = update.message.text.upper()
+    logging.info("Update: " + str(update))
+    logging.info("context: " + str(context))
+    context.bot.send_message(chat_id=update.effective_chat.id, text= reply_message)
+if __name__ == '__main__':
+    main()
